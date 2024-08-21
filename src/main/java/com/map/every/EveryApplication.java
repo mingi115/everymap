23519d54485789752338dc569917843d2bea18dc
mingi <mkultra0115@gmail.com>
2024-08-21 20:25:47 +0900

    초기 프로젝트 등록

---
+++ src/main/java/com/map/every/EveryApplication.java
@@ -0,0 +1,13 @@
+package com.map.every;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EveryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/map/every/EveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.map.every;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
