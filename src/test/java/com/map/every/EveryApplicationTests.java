1303fc7ede7435141c64bcc1c4d64ae64e202264
mingi <mkultra0115@gmail.com>
2024-08-25 03:03:50 +0900

    삭제한 파일에 대한 import수정

--- src/test/java/com/map/every/EveryApplicationTests.java
+++ src/test/java/com/map/every/EveryApplicationTests.java
@@ -1,17 +1,11 @@
 package com.map.every;
 
-import com.map.every.mapper.TestMapper;
-import jakarta.annotation.Resource;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class EveryApplicationTests {
-
-
-
 	@Test
 	void includeIgnoreOverrideTest() {
 	}
-
 }
