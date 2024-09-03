8ac7e4a801169fcd2e3384c2b3bf4ea2e4a06a9c
mingi <mkultra0115@gmail.com>
2024-09-04 01:38:52 +0900

    feat: typing-animation 기능 추가
    - 채팅 로딩 기능 추가
    - chat card dom 생성 함수 추가
    - chat card 추가 기능 추가

--- src/main/resources/static/js/typing-animation.js
+++ src/main/resources/static/js/typing-animation.js
@@ -5,4 +5,70 @@ function animateTyping(messageElementId, message = '', typeSpeed = 10){
 
   message = message.trim().replace(/(?:\r\n|\r|\n)/g, '<br>');
   var typingInstance = new Typed(messageElementId, { strings: [ message ], typeSpeed, showCursor: false, });
+
+
+function createNewChatCard(cardId){
+  const cardBody = document.createElement("div");
+  cardBody.classList.add('card-body');
+  const lxImg = document.createElement("img");
+  lxImg.src = "/image/lx.jpg";
+  lxImg.alt = "avatar 1";
+  lxImg.style.width = "45px";
+  lxImg.style.height = "100%";
+  cardBody.appendChild(lxImg);
+
+  const flexCardBody = document.createElement("div");
+  flexCardBody.classList.add('d-flex');
+  flexCardBody.classList.add('flex-row');
+  flexCardBody.classList.add('justify-content-start');
+  flexCardBody.classList.add('mb-4');
+
+  const card = document.createElement("div");
+  card.classList.add('p-3');
+  card.classList.add('ms-3');
+  card.style.borderRadius = "15px";
+  card.style.backgroundColor = "rgba(1, 152, 129, .2)";
+  const cardContents = document.createElement("p");
+  cardContents.id = `${cardId}`;
+  cardContents.classList.add('small');
+  cardContents.classList.add('mb-0');
+  card.appendChild(cardContents);
+  flexCardBody.appendChild(card);
+  cardBody.appendChild(flexCardBody);
+
+  const chatContainer = document.querySelector("#chat-container");
+  chatContainer.appendChild(cardBody);
+
+  return cardBody;
+}
+
+let loadingChatCard;
+let loadingTyped;
+function addLoadingChatCard(){
+  if(!loadingChatCard){
+    loadingChatCard = createNewChatCard('chat-loading');
+  }
+
+  loadingTyped = new Typed('#chat-loading',
+      { strings: [ '▪▪▪', '▪' ],
+        typeSpeed: 100,
+        showCursor: false,
+        loop:true,
+        startDelay: 0,
+        backDelay: 100,
+        onLastStringBackspaced  : () =>  document.querySelector('#chat-loading').innerHTML = '▪'
+      });
+}
+function deleteLoadingChatCard(){
+  if(loadingChatCard) {
+    loadingTyped.destroy();
+    loadingChatCard.remove()
+  }
+}
+
+let chatCardId=0;
+function addNewChatCard(cardText){
+  chatCardId++;
+  createNewChatCard(`chat-${chatCardId}`);
+  animateTyping(`#chat-${chatCardId}`, cardText, 30);
 }
